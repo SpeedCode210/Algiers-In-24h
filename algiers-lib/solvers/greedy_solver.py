ad87d681349cf2f12e0c850dae6f84d6370da47f
zakaria-raz <mohamed-zakaria.razem@ensia.edu.dz>
2026-04-13 17:25:57 +0100

    greedy solver done

---
+++ algiers-lib/solvers/greedy_solver.py
@@ -0,0 +1,43 @@
+from models.landmark import Landmark
+from models.problem import Problem 
+from models.tour import Tour
+
+class GreedySolver:
+    def __init__(self, problem: Problem, use_ratio: bool = False) -> None:
+        self.problem = problem
+        self.use_ratio = use_ratio
+
+    def _priority(self, candidate: Landmark, curr: Landmark):
+        travel= self.problem.travel_time(curr, candidate)
+        if self.use_ratio:
+            return candidate.interest_score / travel
+        
+        #maximize interest score, if equality minimize travel
+        return (candidate.interest_score, -travel)
+
+    def solve(self) -> Tour:
+        tour = self.problem.create_empty_tour()
+        while True:
+            if tour.visited_landmarks:
+                cur = tour.visited_landmarks[-1]
+            else:
+                cur = self.problem.hotel
+            candidates = self.problem.feasible_candidates(tour)
+
+            invalid: set[str] = set()
+            
+            added = False
+            while candidates:
+                best = max(candidates, key=lambda lm: self._priority(lm, cur))
+                tour.add_landmark(best)
+                if tour.is_valid():
+                    added = True
+                    break
+                tour.remove_landmark(best)
+                invalid.add(best.id)
+                candidates = [c for c in candidates if c.id not in invalid]
+                
+            if not added:
+                break
+            
+        return tour
