e77d86ca36ca3b3c75827fccdea1f4456ce751e1
Aboubakr-ben <aboubakr.bendahmane@ensia.edu.dz>
2026-05-20 10:51:10 +0100

    Update genetic_fitness.py

--- algiers-lib/solvers/genetic_fitness.py
+++ algiers-lib/solvers/genetic_fitness.py
@@ -94,7 +94,7 @@ class ScoreFitnessFunction(FitnessFunction):
         total_reward = sum(
             float(landmark.interest_score) for landmark in tour.visited_landmarks
         )
-        return total_reward**3/ (1 + duration) - invalid # Penalize by dividing by (1 + number of invalid visits)
+        return total_reward**3/ (1 + duration) # Penalize by dividing by (1 + number of invalid visits)
 
 # this is used in the Tailored genetic solver 
 class FeasibilityFitnessFunction(FitnessFunction):
